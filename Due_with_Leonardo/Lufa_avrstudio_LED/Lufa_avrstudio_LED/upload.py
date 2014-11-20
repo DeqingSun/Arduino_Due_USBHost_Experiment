bbb955fdbf4cc05d10c036a352798710b64b5238
Deqing Sun <iamseer@gmail.com>
2014-11-20 17:23:14 -0500

    Use a working library in Atmel studio 6.2

---
+++ Due_with_Leonardo/Lufa_avrstudio_LED/Lufa_avrstudio_LED/upload.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#Filename : helloworld.py
+import os
+
+print 'Hello World'
+
+arduino_avr_path = '/Applications/Arduino.app/Contents/Resources/Java/hardware/tools/avr/'
+
+arduino_port = '/dev/tty.usbmodem141341'
+
+arduino_upload_file = 'Release/Lufa_avrstudio_LED.hex'
+
+os.system(arduino_avr_path+'bin/avrdude -C'+arduino_avr_path+'etc/avrdude.conf -patmega32u4 -cavr109 -P'+arduino_port+' -b57600 -D -Uflash:w:'+arduino_upload_file+':i');
